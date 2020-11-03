0cb647cd92c84e43bb13e6f83f94594d4df5f565
R Brewster <brewster1134+git@gmail.com>
2020-11-03 02:30:16 -0800

    ci: code coverage config update
    # [BLANK LINE]
    # itemized changes
    #
    # TYPES:
    # NO VERSION CHANGE: build, chore, ci, docs, perf, refactor, style, test
    # PATCH BUMP: fix
    # MINOR BUMP: feat
    # MAJOR BUMP: BREAKING CHANGE, [TYPE]!

--- jest.config.js
+++ jest.config.js
@@ -45,7 +45,7 @@ module.exports = {
   coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: ['html-spa', 'text'],
+  coverageReporters: ['html-spa', 'lcovonly', 'text'],
 
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
